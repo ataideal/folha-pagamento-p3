cabf5af97521388bc10aa52144467f6908527d72
Ataide <arpn@ic.ufal.br>
2017-01-23 13:06:41 -0300

    alguns bugs

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -7,17 +8,23 @@ import java.util.Scanner;
 public class main {
 	static class Funcionario
 	{
+		public int sindicato;
+		public String id_sindicato;
+		public Double taxa_sindicato;
 		public Calendar ultimaEntrada;
 		public String nome, endereco, taxas_nome[];
 		public int tipo,horas_trabalhadas[][];// 1-horista 2-assalariado 3-comissionado
-		public Double salario, salario_mensal, comissao,vendas[][],taxas[];
-
+		public Double salario, salario_mensal,salario_hora, comissao,vendas[][],taxas[];
+		public int dia_pag,tipo_pag; //1-semanal 2-bissemanal 3-mensal 
 
 		public Funcionario(){
+			sindicato = 0;
 			horas_trabalhadas = new int[12][31];
 			vendas = new Double[12][31];
 			taxas_nome = new String[10];
 			taxas = new Double[10];
+			dia_pag =6;
+			tipo_pag=1;
 		}
 	};
 
@@ -28,17 +35,21 @@ public class main {
 
 	public static void main(String[] args) {
 		Funcionario funcionarios[] = new Funcionario[20];
+		Funcionario ultimaAlteracao[] = new Funcionario[20];
+		setarFuncionariosIniciais(funcionarios);
 		int totalfuncionarios = 0;
 		int opcao=0;
 		do{
 			opcao = menu();
 			switch(opcao){
 			case 1:
-				if(adicionarFuncionario(funcionarios,totalfuncionarios)==1)
+				undoRedo(ultimaAlteracao,funcionarios);
+				if(adicionarFuncionario(funcionarios)==1)
 					totalfuncionarios++;
 				break;
 			case 2:
 				if(totalfuncionarios>0){
+					undoRedo(ultimaAlteracao,funcionarios);
 					if(removerFuncionario(funcionarios,totalfuncionarios)==1)
 						totalfuncionarios--;
 				}
@@ -46,25 +57,297 @@ public class main {
 					System.out.println("Nao tem funcionarios cadastrados");
 				break;
 			case 3:
+				undoRedo(ultimaAlteracao,funcionarios);
 				lancarCartaoPonto(funcionarios);
+				break;
 			case 4:
+				undoRedo(ultimaAlteracao,funcionarios);
 				baterPonto(funcionarios);
+				break;
 			case 5:
+				undoRedo(ultimaAlteracao,funcionarios);
 				resultadoVenda(funcionarios);
+				break;
 			case 6:
+				undoRedo(ultimaAlteracao,funcionarios);
 				lancarTaxaDeServico(funcionarios);
+				break;
+			case 7:
+				undoRedo(ultimaAlteracao,funcionarios);
+				alterarFuncionario(funcionarios);
+				break;
+			case 8:
+				gerarFolhaPagamento(funcionarios);
+				break;
+			case 9:
+				Funcionario[] aux = new Funcionario[20];
+				for(int i=0;i<20;i++){
+					aux[i] = ultimaAlteracao[i];
+					ultimaAlteracao[i] = funcionarios[i];
+					funcionarios[i] = aux[i];
+				}
+				break;
 			case 10:
 				System.out.println("Saiu do sistema");
-				break;
-			case 11:
-				scan.nextLine();
-				data = scan.nextLine();
 				break;
 			default:
 				System.out.println("Essa opcao nao existe!");
 				break;
 			}
 		}while(opcao!=10);
+	}
+	
+	private static void undoRedo(Funcionario[] ultimaAlteracao, Funcionario[] funcionarios) {
+		for(int i=0;i<20;i++){
+			ultimaAlteracao[i] = funcionarios[i];
+		}
+	}
+
+	private static void setarFuncionariosIniciais(Funcionario[] func) {
+		func[0] = new Funcionario();
+		func[0].nome = "ataide";
+		func[0].endereco = "rua 1";
+		func[0].tipo = 1;
+		func[0].salario_hora = 10.0;
+		func[0].sindicato = 1;
+		func[0].taxa_sindicato = 40.0;
+		func[0].id_sindicato = "000001";
+		func[0].taxas[0]=10.0;
+		func[0].taxas_nome[0]="FGTS";
+		func[0].horas_trabalhadas[0][23-1] = 480;
+		func[0].horas_trabalhadas[0][24-1] = 480;
+		func[0].horas_trabalhadas[0][25-1] = 480;
+		func[0].horas_trabalhadas[0][26-1] = 540;
+		func[0].horas_trabalhadas[0][27-1] = 480;
+		
+		
+	}
+
+	private static void gerarFolhaPagamento(Funcionario[] func) {
+		scan.nextLine();
+		System.out.print("Digite a data da folha: ");
+		String datafolha =scan.nextLine();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Calendar cal = Calendar.getInstance();
+		try {
+			cal.setTime(sdf.parse(datafolha));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		for(int i=0;i<20;i++){
+			if(func[i]!=null){
+				if(func[i].tipo_pag == 3 && func[i].dia_pag==cal.get(Calendar.DAY_OF_MONTH)){
+					pagarFuncionario(func[i],cal);
+					
+				}else if(func[i].tipo_pag==1 && func[i].dia_pag==cal.get(Calendar.DAY_OF_WEEK))
+					pagarFuncionario(func[i],cal);
+				else if(func[i].tipo_pag==2 && func[i].dia_pag==cal.get(Calendar.DAY_OF_WEEK) && cal.get(Calendar.WEEK_OF_YEAR)%2==0){
+					pagarFuncionario(func[i],cal);
+				}
+				
+			}
+		}
+
+	}
+
+	private static void pagarFuncionario(Funcionario func,Calendar cal) {
+		System.out.println("Funcionario: "+func.nome);
+		Calendar aux = cal;
+		func.salario=0.0;
+		if(func.tipo==1){
+			int j=0;
+			if(func.tipo_pag==1){
+				j=7;
+			}else if(func.tipo_pag==2){
+				j=14;
+			}else if(func.tipo_pag==3){
+				aux.add(Calendar.MONTH, -1);
+				j=aux.getActualMaximum(Calendar.DAY_OF_MONTH);
+			}
+			aux = cal;
+			for(int i=0;i<j;i++){
+				aux.add(Calendar.DAY_OF_YEAR, -1);
+				Double horas = (func.horas_trabalhadas[aux.get(Calendar.MONTH)][aux.get(Calendar.DAY_OF_MONTH)-1]/60.0);
+				if(horas>8){
+					func.salario += func.salario_hora * 8;
+					func.salario += func.salario_hora * 1.5 * (horas-8);
+				}else{
+					func.salario += func.salario_hora * horas;
+				}
+			}
+		}else if(func.tipo==2){
+			func.salario = func.salario_mensal;			
+		}else if(func.tipo==3){
+			func.salario = 500.0;
+			int j=0;
+			if(func.tipo_pag==1){
+				j=7;
+			}else if(func.tipo_pag==2){
+				j=14;
+			}else if(func.tipo_pag==3){
+				aux.add(Calendar.MONTH, -1);
+				j=aux.getActualMaximum(Calendar.DAY_OF_MONTH);
+			}
+			aux = cal;
+			for(int i=0;i<j;i++){
+				aux.add(Calendar.DAY_OF_YEAR, -1);
+				Double vendas = (func.vendas[aux.get(Calendar.MONTH)][aux.get(Calendar.DAY_OF_MONTH)-1]);
+				func.salario += vendas*(func.comissao/100);
+			}
+		}
+
+		System.out.println("Salario bruto: "+func.salario);
+		Double taxas=0.0;
+		for(int i=0;i<10;i++){
+			if(func.taxas[i]!=null){
+				System.out.println("Taxa "+func.taxas_nome[i]+": "+func.taxas[i]+"%");
+				taxas += func.salario*(func.taxas[i]/100);
+			}
+		}
+		if(func.sindicato==1){
+			System.out.println("Taxa do sindicato:"+func.taxa_sindicato);
+			taxas += func.taxa_sindicato;
+		}
+
+		System.out.println("Salario liquido: "+(func.salario-taxas));
+		System.out.println("-------------------------------");
+	}
+
+	private static void alterarFuncionario(Funcionario[] func) {
+		listarFuncionariosPorNome(func);
+		System.out.println("Digite o numero do funcionario: ");
+		int id = scan.nextInt();
+		if(func[id]!=null){
+			detalhesFuncionario(func[id]);
+			System.out.println("Digite o que voce deseja alterar: ");
+			int opcao = scan.nextInt();	
+			int flag=1;
+			do{
+				switch(opcao){
+				case 1:
+					System.out.println("Digite o novo nome:");
+					scan.nextLine();
+					func[id].nome = scan.nextLine();
+					break;
+				case 2:
+					System.out.println("Digite o novo endereço:");
+					scan.nextLine();
+					func[id].endereco = scan.nextLine();
+					break;
+				case 3:
+					System.out.println("Digite o novo tipo:");
+					func[id].tipo = scan.nextInt();
+					if(func[id].tipo == 1){
+						System.out.println("Digite o salario-hora:");
+						func[id].salario_hora = scan.nextDouble();
+					}
+					else if(func[id].tipo == 2){
+						System.out.println("Digite o salario-mensal:");
+						func[id].salario_mensal = scan.nextDouble();
+					}
+					else if(func[id].tipo == 3){
+						System.out.println("Digite o percentual de comissao:");
+						func[id].comissao = scan.nextDouble();
+					}
+					break;
+				case 4:
+					if(func[id].tipo == 1){
+						System.out.println("Digite o novo salario-hora:");
+						func[id].salario_hora = scan.nextDouble();
+					}
+					else if(func[id].tipo == 2){
+						System.out.println("Digite o novo salario-mensal:");
+						func[id].salario_mensal = scan.nextDouble();
+					}
+					else if(func[id].tipo == 3){
+						System.out.println("Digite o novo percentual de comissao:");
+						func[id].comissao = scan.nextDouble();
+					}
+					break;
+				case 5:  
+					System.out.println("Digite o novo tipo de pagamento:\n\t1-Semanal\n\t2-Bissemanal\n\t3-Mensal");
+					func[id].tipo_pag = scan.nextInt();
+					if(func[id].tipo_pag == 1 || func[id].tipo_pag == 2)
+						System.out.println("Digite o dia da semana: 1-Dom 2-Seg 3-Ter 4-Quar 5-Qui 6-Sex 7-Sab");
+					if(func[id].tipo_pag == 1 || func[id].tipo_pag == 3)
+						System.out.println("Digite o dia do mes:");
+					func[id].dia_pag = scan.nextInt();
+					break;
+				case 6:
+					System.out.println("Digite se o funcionario faz parte do sindicato:\n1-Sim\n2-Nao");
+					func[id].sindicato = scan.nextInt();
+					if(func[id].sindicato==1){
+						System.out.println("Digite a identificacao no sindicato : ");
+						scan.nextLine();
+						func[id].id_sindicato = scan.nextLine();
+						System.out.println("Taxa do sindicato: "+func[id].taxa_sindicato);
+						func[id].taxa_sindicato = scan.nextDouble();
+					}
+					break;
+				default:
+					System.out.println("Opcao incorreta!");
+					break;	
+				}
+				System.out.println("\nDigite 1 para editar outra informação ou 0 pra voltar ao menu");
+				flag=scan.nextInt();
+			}while(flag!=0);
+		}
+	}
+
+	private static void detalhesFuncionario(Funcionario func) {
+		System.out.println("---Detalhes do funcionario---");
+		System.out.println("1 - Nome: "+func.nome);
+		System.out.println("2- Endereço: "+func.endereco);
+		System.out.print("3- Tipo: ");
+		if(func.tipo==1){
+			System.out.println("Horista");
+			System.out.println("4- Salario Hora:"+func.salario_hora);
+		}
+		if(func.tipo==2){
+			System.out.println("Assalariado");
+			System.out.println("4- Salario Mensal:"+func.salario_mensal);
+		}
+		if(func.tipo==3){
+			System.out.println("Comissionado");
+			System.out.println("4- Percentual comissao:"+func.comissao);
+		}
+		System.out.print("5- Metodo de pagamento: ");
+		if(func.tipo_pag==1)
+			System.out.println("   Semanal "+diaPagamento(func.dia_pag));
+		if(func.tipo_pag==2)
+			System.out.println("   Bissemanal "+diaPagamento(func.dia_pag));
+		if(func.tipo_pag==3) 
+			System.out.println("   Mensal "+func.dia_pag);
+
+		System.out.print("6-Sindicato:");
+		if(func.sindicato==1){
+			System.out.println("\n   Identificacao no sindicato: "+func.id_sindicato);
+			System.out.println("   Taxa do sindicato: "+func.taxa_sindicato);
+		}
+		else{
+			System.out.println("   Nao pertence ao sindicato!");
+		}
+
+	}
+
+	private static String diaPagamento(int dia_pag) {
+		if(dia_pag==Calendar.SUNDAY)
+			return "Domingo";
+		if(dia_pag==Calendar.MONDAY)
+			return "Segunda";
+		if(dia_pag==Calendar.TUESDAY)
+			return "Terça";
+		if(dia_pag==Calendar.WEDNESDAY)
+			return "Quarta";
+		if(dia_pag==Calendar.THURSDAY)
+			return "Quinta";
+		if(dia_pag==Calendar.FRIDAY)
+			return "Sexta";
+		if(dia_pag==Calendar.SATURDAY)
+			return "Sábado";
+
+		return null;
 	}
 
 	private static void lancarTaxaDeServico(Funcionario[] func) {
@@ -76,12 +359,12 @@ public class main {
 		String nomeTaxa = scan.nextLine();	
 		System.out.println("Digite o valor da taxa em porcentagem: ");
 		Double taxa = scan.nextDouble();	
-		
 		for(int i=0;i<10;i++){
-			if(func[id].taxas[i]==0){
+			if(func[id].taxas[i]==null){
 				func[id].taxas[i] = taxa;
 				func[id].taxas_nome[i] = nomeTaxa;
 				System.out.println("Taxa cadastrada com sucesso!");
+				break;
 			}
 		}
 	}
@@ -114,9 +397,9 @@ public class main {
 		System.out.println("----Resultado vendas mes "+mes+" - "+funcionario.nome+"----");
 		for(int i=0;i<31;i++){
 			if(funcionario.vendas[mes][i]!=0)
-			System.out.println("Dia "+i+1+":"+funcionario.vendas[mes][i]+" reais.");
+				System.out.println("Dia "+i+1+":"+funcionario.vendas[mes][i]+" reais.");
 		}
-		
+
 	}
 
 	public static int menu (){
@@ -130,8 +413,7 @@ public class main {
 				+ "7- Alterar detalhes de um empregado\n"
 				+ "8- Rodar a folha de pagamento para hoje\n"
 				+ "9- Undo\\Redo\n"
-				+ "10- Sair\n"
-				+ "10- Mudar data");
+				+ "10- Sair\n");
 		return scan.nextInt();
 	}
 
@@ -174,28 +456,35 @@ public class main {
 		System.out.println("Comissao: "+func.comissao);
 	}
 
-	public static int adicionarFuncionario(Funcionario func[],int totalfuncionarios){
-		System.out.println("Adicionar funcionario");
-		func[totalfuncionarios] = new Funcionario();
-		System.out.println("Nome: ");
-		scan.nextLine();
-		func[totalfuncionarios].nome = scan.nextLine();
-		System.out.println("Endereco: ");
-		func[totalfuncionarios].endereco = scan.nextLine();
-		System.out.println("Tipo: 1-horista 2-assalariado 3-comissionado ");
-		int tipo = scan.nextInt();
-		while(tipo!=1 && tipo!=2 && tipo!=3){
-			System.out.println("Selecione um tipo correto\nTipo: 1-horista 2-assalariado 3-comissionado ");
-			tipo = scan.nextInt();
+	public static int adicionarFuncionario(Funcionario func[]){
+		int j=0;
+		for(int i=0;i<20;i++){
+			if(func[i]==null){
+				j=i;
+				break;
+			}
 		}
 
-		func[totalfuncionarios].tipo = tipo;
-		System.out.println("Salario");
-		func[totalfuncionarios].salario = scan.nextDouble();
-		System.out.println("Salario Mensal: ");
-		func[totalfuncionarios].salario_mensal = scan.nextDouble();
-		System.out.println("Comissao: ");
-		func[totalfuncionarios].comissao = scan.nextDouble();
+		System.out.println("Adicionar funcionario");
+		func[j] = new Funcionario();
+		System.out.println("Nome: ");
+		scan.nextLine();
+		func[j].nome = scan.nextLine();
+		System.out.println("Endereco: ");
+		func[j].endereco = scan.nextLine();
+		System.out.println("Tipo: 1-horista 2-assalariado 3-comissionado ");
+		int tipo = scan.nextInt();
+		func[j].tipo = tipo;
+		if(tipo==1){
+			System.out.println("Salario hora");
+			func[j].salario = scan.nextDouble();
+		}else if(tipo==2){
+			System.out.println("Salario Mensal: ");
+			func[j].salario_mensal = scan.nextDouble();
+		}else if(tipo==3){
+			System.out.println("Porcentagem da comissao: ");
+			func[j].comissao = scan.nextDouble();
+		}
 		return 1;
 	}
 
@@ -235,7 +524,7 @@ public class main {
 		System.out.println("----Dias trabalhados mes "+mes+"----");
 		for(int i=0;i<31;i++){
 			if(funcionario.horas_trabalhadas[mes][i]!=0)
-			System.out.println("Dia "+i+1+":"+funcionario.horas_trabalhadas[mes][i]+" minutos");
+				System.out.println("Dia "+i+1+":"+funcionario.horas_trabalhadas[mes][i]+" minutos");
 		}
 	}
 
